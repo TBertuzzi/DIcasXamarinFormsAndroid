4ca32866681ad33d3b7e5531d5d78addcdabb23e
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-09-13 15:35:29 -0300

    :tada:  Subindo Exemplo

---
+++ DIcasXamarinFormsAndroid.Android/Renderer/CustomEntryRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Android.Content;
+using DIcasXamarinFormsAndroid.Droid.Renderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace DIcasXamarinFormsAndroid.Droid.Renderer
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            Control?.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+    }
+}
---
+++ DIcasXamarinFormsAndroid/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+namespace DIcasXamarinFormsAndroid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            entryPesquisa.On<Android>().SetImeOptions(ImeFlags.Search);
+        }
+    }
+}
